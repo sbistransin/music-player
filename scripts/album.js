f218170f904957e41f92cb301ddfdc3507a73ba4
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-01-27 16:37:23 -0500

    added play and pause button functionality

--- scripts/album.js
+++ scripts/album.js
@@ -7,9 +7,54 @@ var createSongRow = function (songNumber, songName, songLength) {
    + '</tr>'
    ;
 
-   var $row = $(template);
+  var handleSongClick = function() {
 
-   return $row;
+    var clickedSongNumber = $(this).attr('data-song-number');
+    
+    // 1. There is a song that is currently playing
+    if (currentlyPlayingSongNumber !== null) {
+      var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+
+      currentlyPlayingCell.html(currentlyPlayingSongNumber);
+    }
+
+    // 2. There is a song currently playing, but a different one was clicked to play
+    if (clickedSongNumber !== currentlyPlayingSongNumber) {
+      currentlyPlayingSongNumber = clickedSongNumber;
+
+      $(this).html(pauseButtonTemplate);
+    // 3. The currently playing song was clicked
+    } else {
+      currentlyPlayingSongNumber = null;
+      $(this).html(clickedSongNumber);
+    }
+  };
+
+  var onHover = function () {
+    var songItem = $(this).find('.song-item-number');
+    var songNumber = songItem.attr('data-song-number');
+    
+    if (songNumber !== currentlyPlayingSongNumber) {
+      songItem.html(playButtonTemplate);
+    } 
+  };
+  
+  var offHover = function () {
+    var songItem = $(this).find('.song-item-number');
+    var songNumber = songItem.attr('data-song-number');
+  
+    if (songNumber !== currentlyPlayingSongNumber) {
+      songItem.html(songNumber);
+    } 
+  };
+  
+
+  var $row = $(template);
+   
+  $row.find('.song-item-number').click(handleSongClick); 
+  $row.hover(onHover, offHover);
+
+  return $row;
 };
 
 var setCurrentAlbum = function(album) {
@@ -32,6 +77,9 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var currentlyPlayingSongNumber = null;
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 setCurrentAlbum(albums[0]);
 
 
